0f679ba14658da34b3ec8d51a54730988505b801
Lautaro <lautaro.produ@gmail.com>
2021-07-07 20:51:26 -0300

    comienzo archivos switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,20 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesElegido = document.getElementById("txtIdMes").value;
+	switch (mesElegido){
+		
+		case "Enero": alert ("que comienzes bien el año !");
+			break;
+		
+		case "Marzo": alert("a clases !");
+			break;
 
+		case "Julio": alert("se vienen las vacaciones !");
+			break;
+			
+		case "Diciembre": alert("felices fiestas !");
+			break;
 
-
+			default: alert("No se que pasa en este");
+	}
 }//FIN DE LA FUNCIÓN
