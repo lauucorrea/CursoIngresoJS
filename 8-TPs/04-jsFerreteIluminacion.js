ce4d3e88bd89de7fe045ef9d93f381acbf8a1b52
Lautaro <lautaro.produ@gmail.com>
2021-07-19 18:10:13 -0300

    ferrete iluminaciones optimizado

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,47 +1,73 @@
 function CalcularPrecio() {
-  let precioOriginal = 35,
-    precioFinal,
-    impuesto = 1.1,
-    precioFinalImpuesto,
-    cantidadLamparas = parseInt(document.getElementById("txtIdCantidad").value),
-    marcaLamparas = document.getElementById("Marca").value;
+  
+  let cantidadLamparas = parseInt(document.getElementById("txtIdCantidad").value);
 
-  if (cantidadLamparas >= 6) {
-    precioFinal = cantidadLamparas * precioOriginal * 0.5;
-  }
+  if(cantidadLamparas != 0){
 
-  switch (cantidadLamparas) {
-    case 5:
-      if (marcaLamparas == "ArgentinaLuz") {
-        precioFinal = cantidadLamparas * precioOriginal * 0.6;
-      } else {
-        precioFinal = cantidadLamparas * precioOriginal * 0.7;
-      }
+    let precioOriginal = 35,
+      finalPorLampara,
+      precioFinal,
+      impuesto = 1.1,
+      descuento,
+      impuestoBruto,
+      marcaLamparas = document.getElementById("Marca").value;
+
+    switch (cantidadLamparas) {
+      case 5:
+        if (marcaLamparas == "ArgentinaLuz") {
+          descuento = 0.6;
+        } else {
+          descuento = 0.7
+        }
       break;
 
-    case 4:
-      if ( marcaLamparas == "ArgentinaLuz" || marcaLamparas == "FelipeLamparas") {
-        precioFinal = cantidadLamparas * precioOriginal * 0.75;
-      } else {
-        precioFinal = cantidadLamparas * precioOriginal * 0.8;
-      }
+      case 4:
+        if ( marcaLamparas == "ArgentinaLuz" || marcaLamparas == "FelipeLamparas") {
+          descuento = 0.75;
+        } else {
+          descuento = 0.8;
+        }
       break;
 
-    case 3:
-      if (marcaLamparas == "ArgentinaLuz") {
-        precioFinal = cantidadLamparas * precioOriginal * 0.85;
-      } else if (marcaLamparas == "FelipeLamparas") {
-        precioFinal = cantidadLamparas * precioOriginal * 0.9;
-      } else {
-        precioFinal = cantidadLamparas * precioOriginal * 0.95;
-      }
-  }
+      case 3:
+        if (marcaLamparas == "ArgentinaLuz") {
+          descuento = 0.85;
+        } else if (marcaLamparas == "FelipeLamparas") {
+          descuento = 0.9;
+        } else {
+          descuento = 0.95;
+        }
+      break;
+      default:
+        if (cantidadLamparas >= 6) {
+          descuento = 0.5;
+        }else{
+        descuento = 1; //si son menos de 2 lamparas, no hay descuento;
+        }
+    }
+    //calculo el valor unitario con el descuento aplicado
+    // y el precio final para el ticket en el alert
 
-  if (precioFinal >= 120) {
-    precioFinal = precioFinal * impuesto;
-    precioFinalImpuesto = precioFinal * 0.1;
-    alert("IBB Usted pago $" + precioFinal + " siendo $" + precioFinalImpuesto + " el impuesto pagado");
-  }
+    finalPorLampara = precioOriginal * descuento;
+    precioFinal = cantidadLamparas * finalPorLampara;
+    console.log(descuento);
 
-  document.getElementById("txtIdprecioDescuento").value = precioFinal;
+    if (precioFinal >= 120) {
+
+      precioFinal = precioFinal * impuesto;
+      impuestoBruto = precioFinal * 0.1; //impuesto bruto es el 10% del valor del producto
+      document.getElementById("txtIdprecioDescuento").value = "$"+ finalPorLampara + " la unidad";
+      alert("IBB Usted pago $" + precioFinal.toFixed(2) + " siendo $" + impuestoBruto.toFixed(2) + " el impuesto pagado");
+
+    }else{
+      document.getElementById("txtIdprecioDescuento").value = "$"+ finalPorLampara + " la unidad";
+      alert("IBB Usted pago $" + precioFinal.toFixed(2));
+    }
+    
+
+  }
+  else{
+    alert("La cantidad de lamparas ingresadas es inferior al minimo.");
+  }
+  
 }
