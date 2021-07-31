36b4c82b909d2e5987820a22c88da79acc4017a3
Lautaro <lautaro.produ@gmail.com>
2021-07-31 18:54:02 -0300

    dos parciales agregados

--- 9-Parciales/ExamenIngreso2021/uno.js
+++ 9-Parciales/ExamenIngreso2021/uno.js
@@ -1,17 +1,3 @@
-/*
-Debemos realizar la carga de una compra de 5(cinco) productos de desinfección
-de cada una debo obtener los siguientes datos:
-el nombre del producto el tipo de producto (validar "ALCOHOL", "IAC" o "QUAT"),
-el precio (validar entre 100 y 300),
-la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
-el tipo de inflamable("ignífugo", "combustible", "explosivo" ) y la Marca.
-Se debe Informar al usuario lo siguiente:
-a) El promedio de cantidad por tipo de producto
-b) El tipo de inflamable con más cantidad de unidades en total de la compra
-c) Cuántas unidades de IAC con precios menos a 200 (inclusive) se compraron en total
-d) la marca y tipo del más caro de los productos
-Curso de ingreso UTN FRA
-*/
 function mostrar()
 {	
     let productoIngresado,
